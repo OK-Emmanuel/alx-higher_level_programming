dff06138091fb511a5cfa8fe0a5b422346c8be6f
Olawuni Emmanuel Kayode <rexkay1234@gmail.com>
2024-04-09 12:58:38 +0100

    Semi colon observed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let a, b, c
-a = "C is fun"
-b = "Python is cool"
-c = "JavaScript is amazing"
+a = "C is fun";
+b = "Python is cool";
+c = "JavaScript is amazing";
 
-console.log(a)
-console.log(b)
-console.log(c)
+console.log(a);
+console.log(b);
+console.log(c);
